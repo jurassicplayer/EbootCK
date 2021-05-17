f62b34095f046fea06cd03502c678356943cf1b6
jurassicplayer <jurassicplayer75@gmail.com>
2021-05-16 22:09:33 -0700

    Fix: WebKit -> WebEngine, deprecated implicit int
    Changed from WebKit to WebEngine, explicitly asked for integers,
    changed literally every filepath line to use os.path.join instead of
    being silly and hardcoding os separators.

--- EbootCK.py
+++ EbootCK.py
@@ -5,7 +5,7 @@
 import sys, os, shutil, subprocess, re, binascii
 from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog
 import PyQt5.QtWidgets as QtWidgets
-from PyQt5 import QtCore, QtGui, uic, QtWebKitWidgets
+from PyQt5 import QtCore, QtGui, uic, QtWebEngineWidgets
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType("main.ui")
 Ui_PreviewWindow, QtBaseClass = uic.loadUiType("preview.ui")
@@ -28,7 +28,7 @@ class MainWindow(QWidget, Ui_MainWindow):
             shift=[-185,0]
         else:
             shift=[-181, 0]
-        self.move(shift[0] + (resolution.width() / 2) - (self.frameSize().width() / 2), shift[1] + (resolution.height() / 2) - (self.frameSize().height() / 2))
+        self.move(shift[0] + int(resolution.width() / 2) - int(self.frameSize().width() / 2), shift[1] + int(resolution.height() / 2) - int(self.frameSize().height() / 2))
         
         # Connect up the buttons.
         ### PBPTool
@@ -72,31 +72,31 @@ class MainWindow(QWidget, Ui_MainWindow):
         title = self.TitleEdit.text()
         if out_folder and title:
             # Copy all files to directory
-            output_folder = out_folder+'/'+title
+            output_folder = os.path.join(out_folder,title)
             if not os.path.exists(output_folder):
                 os.makedirs(output_folder)
-            files = {'icon0':'/tools/tmp/icon0.png', 
-                     'icon1':'/tools/tmp/icon1.pmf',
-                     'pic0':'/tools/tmp/pic0.png', 
-                     'pic1':'/tools/tmp/pic1.png', 
-                     'snd0':'/tools/tmp/snd0.at3', 
-                     'boot':'/tools/tmp/boot.png'}
+            files = {'icon0':os.path.join('tools','tmp','icon0.png'), 
+                     'icon1':os.path.join('tools','tmp','icon1.pmf'),
+                     'pic0' :os.path.join('tools','tmp','pic0.png'), 
+                     'pic1' :os.path.join('tools','tmp','pic1.png'), 
+                     'snd0' :os.path.join('tools','tmp','snd0.at3'), 
+                     'boot' :os.path.join('tools','tmp','boot.png')}
             for file in files:
                 filename, file_extension = os.path.splitext(files[file])
                 try:
-                    shutil.copy(func.config_folder+files[file], output_folder + '/' + file + file_extension)
+                    shutil.copy(os.path.join(func.config_folder,files[file]), os.path.join(output_folder, file+file_extension))
                 except Exception as e:
                     pass
                     #print(e)
             # Write metadata to readme
-            with open(output_folder+'/'+'README.txt', 'w') as readme:
+            with open(os.path.join(output_folder,'README.txt'), 'w') as readme:
                 readme.write("Title: %s\nVer: %s\nGame: %s\nAuthor: %s\nContact: %s\nComment: %s\n" % (self.TitleEdit.text(), self.VersionEdit.text(), self.GameEdit.text(), self.AuthorEdit.text(), self.ContactEdit.text(), self.CommentEdit.toPlainText()))
             # Archive files into a single file
             shutil.make_archive(output_folder, 'zip', output_folder)
             # Move archive and screenshot to output folder
             shutil.copy(output_folder+'.zip', output_folder)
             os.remove(output_folder+'.zip')
-            func.Take_Screenshot('file:///'+func.config_folder+'/tools/preview.html', output_folder+'/screenshot.png')
+            func.Take_Screenshot(os.path.join('tools','preview.html'), os.path.join(output_folder,'screenshot.png'))
         elif not title and not out_folder:
             self.make_ErrorMessage('An output folder and title have not been set.')
         elif not title:
@@ -233,11 +233,17 @@ class MainWindow(QWidget, Ui_MainWindow):
                 self.setEnabled(False)
                 QApplication.processEvents()
                 if buttonID == "Icon1" and self.FFEditorEdit.text():
-                    # Convert pmf files to .webm for preview
-                    func.PVidConvert(fileName)
+                    if os.path.isfile(self.FFEditorEdit.text()):
+                        # Convert pmf files to .webm for preview
+                        func.PVidConvert(fileName)
+                    else:
+                        self.make_ErrorMessage("Misconfigured FFMpeg path, skipping import conversion.")
                 elif buttonID == "Snd0" and self.At3EditorEdit.text():
-                    # Convert at3 files to .ogg for preview
-                    func.PAudConvert(fileName)
+                    if os.path.isfile(self.FFEditorEdit.text()):
+                        # Convert at3 files to .ogg for preview
+                        func.PAudConvert(fileName)
+                    else:
+                        self.make_ErrorMessage("Misconfigured at3tool path, skipping import conversion.")
                 self.setEnabled(True)
                 EditEntry.setText(fileName)
                 if self.TabContainer.currentIndex() == 0:
@@ -272,22 +278,24 @@ class PreviewWindow(QWidget, Ui_PreviewWindow):
         
         # Make local modifications
         resolution = QtWidgets.QDesktopWidget().screenGeometry()
-        self.move(240 + (resolution.width() / 2) - (self.frameSize().width() / 2),(resolution.height() / 2) - (self.frameSize().height() / 2))
+        self.move(int(240 + (resolution.width() / 2) - (self.frameSize().width() / 2)),int((resolution.height() / 2) - (self.frameSize().height() / 2)))
         self.setWindowFlags(QtCore.Qt.WindowTitleHint | QtCore.Qt.CustomizeWindowHint)
-        self.webView.setUrl(QtCore.QUrl('file:///'+func.config_folder+'/tools/preview.html'))
+        self.webView.load(QtCore.QUrl("file:///"+os.path.join(func.config_folder,'tools','preview.html').replace("\\", "/")))
+        self.webView.show()
         self.show()
+
     
     def update_preview(self, icon0, icon1, pic0, pic1, snd0, boot):
         func = Functions()
         ## Copy files to tmp folder
-        tmp_folder = func.config_folder +"/tools/tmp/"
+        tmp_folder = os.path.join(func.config_folder,"tools","tmp")
         if not os.path.exists(tmp_folder):
             os.makedirs(tmp_folder)
         files = {'icon0':icon0, 'icon1':icon1, 'pic0':pic0, 'pic1':pic1, 'snd0':snd0, 'boot':boot}
         for file in files:
             filename, file_extension = os.path.splitext(files[file])
             try:
-                shutil.copy(files[file], tmp_folder + file + file_extension)
+                shutil.copy(files[file], os.path.join(tmp_folder,file+file_extension))
             except Exception as e:
                 pass
                 #print(e)
@@ -309,7 +317,7 @@ class ErrorMessage(QWidget, Ui_ErrorMessage):
         # Make local modifications
         resolution = QtWidgets.QDesktopWidget().screenGeometry()
         self.setWindowFlags(QtCore.Qt.WindowTitleHint | QtCore.Qt.CustomizeWindowHint)
-        self.move((resolution.width() / 2) - (self.frameSize().width() / 2),(resolution.height() / 2) - (self.frameSize().height() / 2))
+        self.move(int((resolution.width() / 2) - (self.frameSize().width() / 2)),int((resolution.height() / 2) - (self.frameSize().height() / 2)))
         
         # Connect button
         self.pushButton.clicked.connect(self.closeEvent)
@@ -332,7 +340,7 @@ class Functions():
     def __init__(self):
         # Get local settings
         self.config_folder = os.getcwd()
-        self.tmp_folder = self.config_folder+'/tools/tmp'
+        self.tmp_folder = os.path.join(self.config_folder,"tools","tmp")
         self.config = self.LoadOpts()
         
     def SaveOpts(self, ff_opts='-an -c:v rawvideo -y -vf scale=144:80,setsar=1/1,setdar=9/5', audedit='', imgedit='', videdit='', at3tool='', ffmpeg='', umdedit='', output='', author='', contact='', comment=''):
@@ -355,11 +363,11 @@ class Functions():
                                   'image_editor': '/usr/bin/gimp',
                                   'video_editor': '/usr/bin/lightworks',
                                   'ffmpeg': '/usr/bin/ffmpeg'})
-        with open(self.config_folder+'/EbootCK.ini', 'w') as configfile:
+        with open(os.path.join(self.config_folder,'EbootCK.ini'), 'w') as configfile:
             config.write(configfile)
     def LoadOpts(self):
         config = configparser.ConfigParser()
-        configfile = self.config_folder+'/EbootCK.ini'
+        configfile = os.path.join(self.config_folder,'EbootCK.ini')
         if not os.path.exists(self.tmp_folder):
             os.makedirs(self.tmp_folder)
         if os.path.exists(configfile):
@@ -370,7 +378,7 @@ class Functions():
         return config
     def Take_Screenshot(self, url, outfile):
         output = outfile
-        webViewport = QtWebKitWidgets.QWebView()
+        webViewport = QtWebEngineWidgets.QWebView()
         webViewport.resize(480, 272)
         webViewport.load(QtCore.QUrl(url))
         webViewport.loadFinished.connect(self._loadFinished(webViewport, output))
@@ -390,7 +398,7 @@ class Functions():
             webViewport.destroy()
         return web_to_image
     def At3Convert(self, InFile, OutFile):
-        TmpFile = self.tmp_folder+'/audio.wav'
+        TmpFile = os.path.join(self.tmp_folder,'audio.wav')
         # Convert original to wav
         command = '{0} -i "{1}" -y -c:a pcm_s16le -vn -ar 44100 "{2}"'.format(self.config['Settings']['ffmpeg'], str(InFile), str(TmpFile))
         subprocess.call(command, shell=True, stdout=subprocess.PIPE)
@@ -402,7 +410,6 @@ class Functions():
         if os.name == 'posix':
             ## Add at3tool wine wrapper
             command = 'wine {0}'.format(command)
-        print(command)
         at3convert = subprocess.call(command, shell=True)
         self.PAudConvert(OutFile)
         os.remove(TmpFile)
@@ -420,12 +427,13 @@ class Functions():
             pmf_file = open(OutFile, "w+b")
             pmf_file.write(binascii.unhexlify(pmf_header)+f.read())
     def PVidConvert(self, InFile):
-        OutFile = self.tmp_folder+'/video.mp4'
-        command = '"{0}" -i "{1}" -y -an -qmax 25 -threads 2 "{2}"'.format(self.config['Settings']['ffmpeg'], str(InFile), str(OutFile))
+        OutFile = os.path.join(self.tmp_folder, 'video.webm')
+        command = '"{0}" -i "{1}" -y "{2}"'.format(self.config['Settings']['ffmpeg'], str(InFile), str(OutFile))
+        print(command)
         ffconvert = subprocess.call(command, shell=True)
     def PAudConvert(self, InFile):
-        OutFile = self.tmp_folder+'/audio.ogg'
-        TmpFile = self.tmp_folder+'/snd0.wav'
+        OutFile = os.path.join(self.tmp_folder, 'audio.ogg')
+        TmpFile = os.path.join(self.tmp_folder, 'snd0.wav')
         # Convert At3 to wav
         command = '"{0}" -repeat 1 -d "{1}" "{2}"'.format(self.config['Settings']['at3tool'], str(InFile), str(TmpFile))
         if os.name == 'posix':
@@ -440,10 +448,10 @@ class Functions():
     def get_mps_info(self, InFile):
         mps_size = hex(os.path.getsize(InFile))[2:].rjust(8,"0")
         if os.name == 'nt':
-            ffprobe_exec = '/ffprobe.exe'
+            ffprobe_exec = 'ffprobe.exe'
         elif os.name == 'posix':
-            ffprobe_exec = '/ffprobe'
-        ffprobe = os.path.dirname(self.config['Settings']['ffmpeg'])+ffprobe_exec
+            ffprobe_exec = 'ffprobe'
+        ffprobe = os.path.join(os.path.dirname(self.config['Settings']['ffmpeg']),ffprobe_exec)
         ffprocess = subprocess.Popen([ffprobe, '-i', InFile], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
         for x in ffprocess.stdout.readlines():
             if "Stream" in str(x):
